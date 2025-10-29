31bd7a425a2d02a561426b488c7096f5c47007d8
Mugunthan S <smnmugunthan@gmail.com>
2025-10-29 21:07:42 +0530

    Initial commit - Bike Tracker

--- js/issues.js
+++
@@ -1,6 +0,0 @@
-window.IssuesModule = () => {
-  // Issue tracking implementation
-  return <div>Issues Module - Track bike problems</div>;
-};
-
-console.log('✅ Issues loaded');
--- js/service.js
+++
@@ -1,6 +0,0 @@
-window.ServiceModule = () => {
-  // Service management implementation
-  return <div>Service Module - Track maintenance</div>;
-};
-
-console.log('✅ Service loaded');
